015ac95399161f7d926f316e2c8bf08de54864c1
EugeneZhdanov <jeka7753191@gmail.com>
2022-01-21 08:25:42 +0200

    всі остальні елементи по модалках

---
+++ src/components/UserLogout/UserLogout.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import Modal from '../Modal';
+import { logOut } from '../../redux/auth';
+import useWindowDimensions from '../../hooks/useWindowDimensions';
+import logOutImg from '../../img/svg/logOutSprite.svg';
+
+import s from './UserLogout.module.css';
+
+const UserLogout = () => {
+  const dispatch = useDispatch();
+
+  const toggleModal = () => {
+    setShowModal(prevShowModal => !prevShowModal);
+  };
+
+  const logoutModal = () => {
+    dispatch(logOut());
+    toggleModal();
+  };
+
+  const [setModalOpen, setShowModal] = useState(false);
+
+  const viewPort = useWindowDimensions();
+
+  return (
+    <>
+      {viewPort.width >= 768 && (
+        <button type="button" onClick={toggleModal} className={s.logoutBtn}>
+          <p className={s.textBtn}>Выйти</p>
+        </button>
+      )}
+      {viewPort.width < 768 && (
+        <button type="button" onClick={toggleModal} className={s.btnLogout}>
+          <svg className={s.iconLogout}>
+            <use href={`${logOutImg}#logOut`} />
+          </svg>
+        </button>
+      )}
+      {setModalOpen && (
+        <Modal
+          text={'Вы действительно хотите выйти?'}
+          handleClickLeft={logoutModal}
+          handleClickRight={toggleModal}
+          onClose={toggleModal}
+        />
+      )}
+    </>
+  );
+};
+
+export default UserLogout;
