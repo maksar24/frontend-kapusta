f09908805e742bef00c6b582fb13f1f873715dd6
Leskovets Maksym <leskovecmaksim@gmail.com>
2022-01-26 13:10:36 +0100

    fix5

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -14,8 +14,7 @@ export default function AppBar() {
         <a href="/">
           <Icons iconName="miniLogo" />
         </a>
-        <Navigation />
-        {/* {isLoggedIn && <Navigation />} */}
+        {isLoggedIn && <Navigation />}
       </div>
     </header>
   );
