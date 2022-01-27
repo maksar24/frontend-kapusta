60a4dfd9ef1b0c0480b8ad7e6008d76d0bd5e3b7
Leskovets Maksym <leskovecmaksim@gmail.com>
2022-01-27 23:50:10 +0100

    PrivetRoute

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import AppBar from './components/AppBar/AppBar';
@@ -6,6 +6,23 @@ import ReportPage from './pages/ReportPage';
 import HomePage from './pages/HomePage';
 import CommonPage from './pages/CommonPage';
 import AddTransactionView from './views/AddTransactionView';
+// import PrivetRoute from './components/PrivetRoute';
+
+// const AuthView = lazy(() =>
+//   import('./pages/HomePage' /* webpackChunkName: "authentication-page" */),
+// );
+
+// const MainView = lazy(() =>
+//   import('./pages/CommonPage' /* webpackChunkName: "main-page" */),
+// );
+
+// const MobilTransactionView = lazy(() =>
+//   import('./views/AddTransactionView' /* webpackChunkName: "mobil-tr-page" */),
+// );
+
+// const ReportView = lazy(() =>
+//   import('./pages/ReportPage' /* webpackChunkName: "report-page" */),
+// );
 
 function App() {
   return (
@@ -21,6 +38,37 @@ function App() {
         />
         <Route path="report" element={<ReportPage />} />
       </Routes>
+      {/* <AppBar />
+        <Suspense fallback={'Loading...'}>
+          <Routes>
+            <Route
+              path='/'
+              element={
+                <AuthView />
+              }
+            />
+            <Route
+              path='/balance'
+              element={
+                <PrivetRoute>
+                  <MainView />
+                </PrivetRoute>
+              }
+            />
+            <Route
+              path='/balance/addViaMobile'
+              element={
+                <MobilTransactionView />
+              }
+            />
+            <Route
+              path='/report'
+              element={
+                <ReportView />
+              }
+            />
+          </Routes>
+        </Suspense> */}
     </>
   );
 }
