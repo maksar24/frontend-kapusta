10214aecaef096c2230602b16afef5a62576b832
MaxymKucherenko <maxym_88@ukr.net>
2022-02-07 00:28:54 +0200

    summary fix

--- src/components/IncomeOutcomeButtons/IncomeOutcomeButtons.js
+++ src/components/IncomeOutcomeButtons/IncomeOutcomeButtons.js
@@ -1,30 +1,76 @@
-import { Fragment, useState } from 'react';
+import { Fragment, useState, useEffect } from 'react';
 import Media from 'react-media';
 
 import styles from './IncomeOutcomeButtons.module.css';
+
+import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux';
+import { getToken } from '../../redux/auth/auth-selectors';
+
+import { fetchSuccess, fetchError, summary } from '../../redux/balance/index';
 
 const IncomeOutcomeButtons = ({
   transactionType,
   toggleTransactionType,
   showMobileAddView,
 }) => {
-  const type = transactionType;
+  /* const type = transactionType; */
 
   const [outcomeActive, setOutcomeActive] = useState(true);
   const [incomeActive, setIncomeActive] = useState(false);
+  const [type, seType] = useState('consumption');
+  const [thisYear, setThisYear] = useState(2022);
+  const { data } = useSelector(data => data.balanceReducer);
+  const token = useSelector(getToken);
+  const dispatch = useDispatch();
+  const [isLoading, setLoading] = useState(false);
 
   const toggleActive = () => {
     if (incomeActive) {
       setOutcomeActive(true);
       setIncomeActive(false);
-      return;
+      seType(`consumption`);
     }
 
     if (outcomeActive) {
       setIncomeActive(true);
       setOutcomeActive(false);
+      seType(`income`);
     }
   };
+
+  const fethSummary = async type => {
+    let config = {
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    };
+    try {
+      setLoading(true);
+      const response = await axios.get(
+        `/transaction/summary/${type}/${thisYear}`,
+        config,
+      );
+      dispatch(fetchSuccess(response.data));
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      dispatch(fetchError(error.message));
+    }
+  };
+
+  useEffect(() => {
+    fethSummary(type);
+  }, [thisYear, type, dispatch]);
+
+  useEffect(() => {
+    if (data) {
+      dispatch(summary(data.summary));
+    }
+  }, [isLoading]);
+
+  console.log(type);
+  console.log(data);
 
   const changeType = () => {
     toggleTransactionType();
@@ -71,18 +117,18 @@ const IncomeOutcomeButtons = ({
             <Fragment>
               <button
                 className={`${styles.typeButton}
-               ${type === 'outcome' && styles.isActive}`}
+               ${outcomeActive && styles.isActive}`}
                 type="button"
-                onClick={(toggleActive, changeType)}
+                onClick={toggleActive}
               >
                 РАСХОД
               </button>
 
               <button
                 className={`${styles.typeButton}
-               ${type === 'income' && styles.isActive}`}
+               ${incomeActive && styles.isActive}`}
                 type="button"
-                onClick={(toggleActive, changeType)}
+                onClick={toggleActive}
               >
                 ДОХОД
               </button>
